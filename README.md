036e0b0f85e31798fdd43ff003b05933a8a930c0
EQuimper <quimperemanuel@gmail.com>
2017-05-11 23:43:45 -0400

    Add example and readme

--- README.md
+++ README.md
@@ -1,5 +1,68 @@
+# @appandflow/touchable
+
+An helper to react-native touchable element. Work with all the feedback provided by react-native and add the ripple effect for android by default.
+
+## Why use this library?
+
+Let you import only one component and don't care about the platform. Also get native feedback by default for android. Don't worry you can disabled this effect by passing `native={false}`
+
 ## Installation
 
 `yarn add @appandflow/touchable`
 
 ## Usage
+
+`import Touchable from '@appandflow/touchable';`
+
+Touchable become like a view and have access to all his props.
+
+## Props
+
+### feedback
+
+- opacity
+- highlight
+- none
+
+### disabled
+
+You can disabled the touch by passing disabled true. Default false.
+
+### native [Android only]
+
+For don't get the native effect just pass native false. By default this is true.
+
+**If pass false don't forget to pass a feedback**
+
+## Example
+
+Play with on [Expo](https://exp.host/@equimper/example)
+
+Take a look at example folder also. [Link](https://github.com/AppAndFlow/react-native-touchable/blob/master/example)
+
+```js
+import React, { Component } from 'react';
+import { Text, View, Alert } from 'react-native';
+import Touchable from '@appandflow/touchable';
+
+class App extends Component {
+  _handleAlert = feedback => {
+    Alert.alert(`You touch the feedback: ${feedback}`)
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Touchable feedback="opacity" style={[styles.button, styles.opacity]} onPress={() => this._handleAlert('opacity')}>
+          <Text style={styles.buttonText}>OPACITY</Text>
+        </Touchable>
+        <Touchable feedback="highlight" style={[styles.button, styles.highlight]} onPress={() => this._handleAlert('highlight')}>
+          <Text style={styles.buttonText}>HIGHLIGHT</Text>
+        </Touchable>
+        <Touchable feedback="none" style={[styles.button, styles.none]} onPress={() => this._handleAlert('none')}>
+          <Text style={styles.buttonText}>NONE</Text>
+        </Touchable>
+      </View>
+    );
+  }
+}
+```
--- example/main.js
+++ example/main.js
@@ -1,23 +1,89 @@
 import Expo from 'expo';
-import React from 'react';
-import { StyleSheet, Text, View, Alert } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Alert, Platform } from 'react-native';
 import Touchable from '@appandflow/touchable';
 
-class App extends React.Component {
+const isAndroid = Platform.OS === 'android';
+
+class App extends Component {
   _handleAlert = feedback => {
-    Alert.alert(`You touch the feedback: ${feedback}`)
-  }
+    Alert.alert(`You touch the feedback: ${feedback}`);
+  };
   render() {
+    if (isAndroid) {
+      return (
+        <View style={styles.container}>
+          <Text style={styles.androidOnlyText}>Android Only</Text>
+          <Touchable
+            native={false}
+            feedback="opacity"
+            style={[styles.button, styles.opacity]}
+            onPress={() => this._handleAlert('native false with opacity')}
+          >
+            <Text style={styles.buttonText}>OPACITY</Text>
+          </Touchable>
+          <Touchable
+            feedback="highlight"
+            native={false}
+            style={[styles.button, styles.highlight]}
+            onPress={() => this._handleAlert('native fals with highlight')}
+          >
+            <Text style={styles.buttonText}>HIGHLIGHT</Text>
+          </Touchable>
+          <Touchable
+            feedback="none"
+            style={[styles.button, styles.none]}
+            onPress={() => this._handleAlert('native false with none')}
+          >
+            <Text style={styles.buttonText}>NONE</Text>
+          </Touchable>
+          <Touchable
+            feedback="opacity"
+            disabled
+            style={[styles.button, styles.disabled]}
+            onPress={() => this._handleAlert('disabled')}
+          >
+            <Text style={styles.buttonText}>DISABLED</Text>
+          </Touchable>
+          <Touchable
+            style={[styles.button, styles.notDisabled]}
+            onPress={() => this._handleAlert('native not disabled')}
+          >
+            <Text style={styles.buttonText}>NATIVE NOT DISABLED</Text>
+          </Touchable>
+        </View>
+      );
+    }
     return (
       <View style={styles.container}>
-        <Touchable feedback="opacity" style={[styles.button, styles.opacity]} onPress={() => this._handleAlert('opacity')}>
+        <Touchable
+          feedback="opacity"
+          style={[styles.button, styles.opacity]}
+          onPress={() => this._handleAlert('opacity')}
+        >
           <Text style={styles.buttonText}>OPACITY</Text>
         </Touchable>
-        <Touchable feedback="highlight" style={[styles.button, styles.highlight]} onPress={() => this._handleAlert('highlight')}>
+        <Touchable
+          feedback="highlight"
+          style={[styles.button, styles.highlight]}
+          onPress={() => this._handleAlert('highlight')}
+        >
           <Text style={styles.buttonText}>HIGHLIGHT</Text>
         </Touchable>
-        <Touchable feedback="none" style={[styles.button, styles.none]} onPress={() => this._handleAlert('none')}>
+        <Touchable
+          feedback="none"
+          style={[styles.button, styles.none]}
+          onPress={() => this._handleAlert('none')}
+        >
           <Text style={styles.buttonText}>NONE</Text>
+        </Touchable>
+        <Touchable
+          feedback="opacity"
+          disabled
+          style={[styles.button, styles.disabled]}
+          onPress={() => this._handleAlert('disabled')}
+        >
+          <Text style={styles.buttonText}>DISABLED</Text>
         </Touchable>
       </View>
     );
@@ -31,21 +97,25 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  androidOnlyText: {
+    fontSize: 20,
+    fontWeight: '700',
+  },
   button: {
     height: 50,
     width: 250,
     justifyContent: 'center',
     alignItems: 'center',
-    marginVertical: 30,
+    marginVertical: 10,
     elevation: 1,
     shadowColor: '#000',
     shadowOpacity: 0.3,
     shadowOffset: {
       width: 2,
-      height: 2
+      height: 2,
     },
     shadowRadius: 12,
-    borderRadius: 5
+    borderRadius: 5,
   },
   buttonText: {
     fontSize: 20,
@@ -53,14 +123,23 @@ const styles = StyleSheet.create({
     color: '#000',
   },
   opacity: {
-    backgroundColor: 'cyan'
+    backgroundColor: 'cyan',
   },
   highlight: {
-    backgroundColor: 'turquoise'
+    backgroundColor: 'blue',
   },
   none: {
-    backgroundColor: 'gray'
-  }
+    backgroundColor: 'gray',
+  },
+  notDisabled: {
+    backgroundColor: 'red',
+  },
+  disabled: {
+    backgroundColor: 'green',
+  },
+  nativeDisabled: {
+    backgroundColor: 'purple',
+  },
 });
 
 Expo.registerRootComponent(App);
