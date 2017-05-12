5324e82a496f199bd1c9ec737a2449ae18f8b5ea
EQuimper <quimperemanuel@gmail.com>
2017-05-11 22:48:09 -0400

    Update Example

--- example/main.js
+++ example/main.js
@@ -1,13 +1,24 @@
 import Expo from 'expo';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-// import Touchable from '@appandflow/touchable';
+import { StyleSheet, Text, View, Alert } from 'react-native';
+import Touchable from '@appandflow/touchable';
 
 class App extends React.Component {
+  _handleAlert = feedback => {
+    Alert.alert(`You touch the feedback: ${feedback}`)
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up main.js to start working on your app!</Text>
+        <Touchable feedback="opacity" style={[styles.button, styles.opacity]} onPress={() => this._handleAlert('opacity')}>
+          <Text style={styles.buttonText}>OPACITY</Text>
+        </Touchable>
+        <Touchable feedback="highlight" style={[styles.button, styles.highlight]} onPress={() => this._handleAlert('highlight')}>
+          <Text style={styles.buttonText}>HIGHLIGHT</Text>
+        </Touchable>
+        <Touchable feedback="none" style={[styles.button, styles.none]} onPress={() => this._handleAlert('none')}>
+          <Text style={styles.buttonText}>NONE</Text>
+        </Touchable>
       </View>
     );
   }
@@ -20,6 +31,36 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button: {
+    height: 50,
+    width: 250,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginVertical: 30,
+    elevation: 1,
+    shadowColor: '#000',
+    shadowOpacity: 0.3,
+    shadowOffset: {
+      width: 2,
+      height: 2
+    },
+    shadowRadius: 12,
+    borderRadius: 5
+  },
+  buttonText: {
+    fontSize: 20,
+    fontWeight: '700',
+    color: '#000',
+  },
+  opacity: {
+    backgroundColor: 'cyan'
+  },
+  highlight: {
+    backgroundColor: 'turquoise'
+  },
+  none: {
+    backgroundColor: 'gray'
+  }
 });
 
 Expo.registerRootComponent(App);
